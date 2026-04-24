293f676f37f5146f9a71dcf455ee06ccb7eeb461
znhw <zenhew@yahoo.com>
2026-04-24 10:26:00 +0800

    update next config output

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  output: "export", // 👈 tells Next to generate static HTML
-  images: {
-    unoptimized: true, // 👈 required for GitHub Pages
-  },
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   output: "export", // 👈 tells Next to generate static HTML
+//   images: {
+//     unoptimized: true, // 👈 required for GitHub Pages
+//   },
+// };
 
-export default nextConfig;
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+}
+
+module.exports = nextConfig
